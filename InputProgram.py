05b3289ab8d865133a75438da8543634aae1d60b
Joshua Greenert <42500582+joshgreenert@users.noreply.github.com>
2022-03-29 19:22:12 -0400

    Create InputProgram.py

---
+++ InputProgram.py
@@ -0,0 +1,19 @@
+# DSC 510
+# Week 3
+# Programming Assignment Week 2
+# Author: Joshua Greenert
+# Date: 3/24/2022
+# This program will welcome the user, retrieve the company name from the user,
+# calculate the installation of fiber optic cable by multiplying the total cost
+# as number of feet by $0.87, print a receipt for the user including the number
+# of feet, the company name, the calculated cost, and total cost in legible format.
+print("***************************************************")
+print("Welcome to the Fiber Optic Cost Calculator Program!")
+print("***************************************************")
+
+# Ask the user for the company name.
+print("Please enter the name of your company: ")
+companyName = input()
+
+
+
