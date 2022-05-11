726def830f56745222fde8904184ffdaa31e18e6
Joshua Greenert <42500582+joshgreenert@users.noreply.github.com>
2022-05-10 21:14:31 -0400

    Create WritingFileOperations.py

---
+++ WritingFileOperations.py
@@ -0,0 +1,22 @@
+# DSC 510
+# Week 9
+#
+# Programming Assignment Week 9
+# Author: Joshua Greenert
+# Date: 5/10/2022
+#
+# For this week we will modify our Gettysburg processing program from week 8 in order to generate a 
+# text file from the output rather than printing to the screen. Your program should have a new function 
+# called process_file which prints to the file (this method should almost be the same as the pretty_print 
+# function from last week. Keep in mind that we have print statements in main as well. Your program must 
+# modify the print statements from main as well.
+
+
+# Your program must have a header. 
+# Your program should adhere to PEP8 guidelines especially as it pertains to variable names.
+# Create a new function called process_fie. This function will perform the same operations as pretty_print from week 8 however it will print to a file instead of to the screen.
+# Modify your main method to print the length of the dictionary to the file as opposed to the screen.
+# This will require that you open the file twice. Once in main and once in process_file.
+# Prompt the user for the filename they wish to use to generate the report.
+# Use the filename specified by the user to write the file.
+# This will require you to pass the file as an additional parameter to your new process_file function.
