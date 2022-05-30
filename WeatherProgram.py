48c9e8fe391852bdb9138a642be8ae57bcc59b59
Joshua Greenert <42500582+joshgreenert@users.noreply.github.com>
2022-05-30 11:00:13 -0400

    Update WeatherProgram.py

--- WeatherProgram.py
+++ WeatherProgram.py
@@ -13,19 +13,20 @@ import time
 
 # Define the print function for the multi-dimensional array.
 def print_weather_data(weatherDataArray):
-    print(f"The forcast for {weatherDataArray[0][1]} on {weatherDataArray[0][0]} is {weatherDataArray[0][2]}")
+    print(f"\nThe forcast for {weatherDataArray[0][1]} on {weatherDataArray[0][0]} is {weatherDataArray[0][2]}")
     print(f"There will be a high of {weatherDataArray[0][3]} and a low of {weatherDataArray[0][4]}.")
     print(f"Pressure: {weatherDataArray[0][5]}")
     print(f"Humidity: {weatherDataArray[0][6]}")
     print(f"Cloud Cover: {weatherDataArray[0][7]}")
+    print(f"Description: {weatherDataArray[0][8]}\n")
 
 # Define the zip code call to the API.
 def get_zipCode_json():
-    zipCode = input("Please enter the zip code you would like:")
+    zipCode = input("Please enter the zip code you would like: ")
 
     while (zipCode.isnumeric() != True and len(zipCode) != 5):
         print("Error: Invalid selection")
-        zipCode = input("Please enter the zip code you would like:")
+        zipCode = input("Please enter the zip code you would like: ")
 
     tempUnit = input("Would you like to view temperatures in Fahrenheit, Celsius, or Kelvin.\nEnter 'F' for Fahrenheit, 'C' for Celsius, 'K' for Kelvin:")
 
@@ -45,7 +46,12 @@ def get_zipCode_json():
     appId = "73545cb70bb2e48c60e5a4d09cf7fd5a"
 
     geoEndPoint = "http://api.openweathermap.org/geo/1.0/zip?zip=" + zipCode + "&appid=" + appId
-    response = requests.get(geoEndPoint)
+
+    try:
+        response = requests.get(geoEndPoint)
+    except requests.exceptions.ConnectionError: 
+        print("ERROR: Could not connect to API service.\nPlease check your Internet connection and try again.")
+        get_zipCode_json()
 
     # Get the response from the geocode request.
     geoJsonResponse = response.json() 
@@ -57,7 +63,11 @@ def get_zipCode_json():
     currentWeatherEndPoint = "https://api.openweathermap.org/data/2.5/weather?appid=" + str(appId) + "&units=" + str(tempUnit) + "&lat=" + str(lat) + "&lon=" + str(lon)
 
     # Get the response from the geocode request.
-    response = requests.get(currentWeatherEndPoint) 
+    try:
+        response = requests.get(currentWeatherEndPoint) 
+    except requests.exceptions.ConnectionError:
+        print("ERROR: Could not connect to API service.\nPlease check your Internet connection and try again.")
+        get_zipCode_json()
 
     # Get the response from the geocode request.
     currentWeatherJsonResponse = response.json()
@@ -87,7 +97,12 @@ def get_city_json():
     appId = "73545cb70bb2e48c60e5a4d09cf7fd5a"
 
     geoEndPoint = "http://api.openweathermap.org/geo/1.0/direct?q={" + cityName + "," + stateName + "}&appid=" + appId
-    response = requests.get(geoEndPoint)
+
+    try:
+        response = requests.get(geoEndPoint)
+    except requests.exceptions.ConnectionError: 
+        print("ERROR: Could not connect to API service.\nPlease check your Internet connection and try again.")
+        get_city_json()
 
     # Get the response from the geocode request.
     geoJsonResponse = response.json() 
@@ -99,7 +114,11 @@ def get_city_json():
     currentWeatherEndPoint = "https://api.openweathermap.org/data/2.5/weather?appid=" + str(appId) + "&units=" + str(tempUnit) + "&lat=" + str(lat) + "&lon=" + str(lon)
 
     # Get the response from the geocode request.
-    response = requests.get(currentWeatherEndPoint) 
+    try:
+        response = requests.get(currentWeatherEndPoint) 
+    except requests.exceptions.ConnectionError: 
+        print("ERROR: Could not connect to API service.\nPlease check your Internet connection and try again.")
+        get_city_json()
 
     # Get the response from the geocode request.
     currentWeatherJsonResponse = response.json()
@@ -122,17 +141,13 @@ def get_weather_data(jsonResponse):
     lowTemp = jsonResponse['main']['temp_min']
     pressure = jsonResponse['main']['pressure']
     humidity = jsonResponse['main']['humidity']
-    cloudCover = jsonResponse['weather'][0]['description']
+    cloudCover = jsonResponse['weather'][0]['main']
+    description = jsonResponse['weather'][0]['description']
 
     # Add the items to the weather data array.
-    weatherDataArray.append([convertedDate, cityName, currentTemp, highTemp, lowTemp, pressure, humidity, cloudCover])
+    weatherDataArray.append([convertedDate, cityName, currentTemp, highTemp, lowTemp, pressure, humidity, cloudCover, description])
 
     return weatherDataArray
-
-# Define the method that returns a city API object array.
-def get_weather_data_city(jsonResponse):
-    # Get the count of days
-    weatherDataArray = []
 
 # Start the main program here.
 def main():
@@ -142,26 +157,49 @@ def main():
     jsonResponse = ""
 
     # Welcome the user and get the user's weather lookup request.
-    print("###################################")
-    print("Welcome to the weather application!")
-    print("###################################")
+    print("############################################")
+    print("     Welcome to the Weather Application!    ")
+    print("############################################")
 
-    zipOrCityPrompt = input("Would you like to lookup weather data by US City or zip code?\nEnter 1 for US City 2 for zip:")
+    # Ask the user if they would like to use the application.
+    userInput = input("Would you like to look up weather information today?\nPress 'Y' or 'y' for yes or press any other key to quit. ")
 
-    while zipOrCityPrompt != "1" and zipOrCityPrompt != "2":
-        print("Error: Invalid selection")
-        zipOrCityPrompt = input("Would you like to lookup weather data by US City or zip code?\nEnter 1 for US City 2 for zip:")
+    while(userInput.lower() == 'y'):
+        zipOrCityPrompt = input("Would you like to lookup weather data by US City or zip code?\nEnter 1 for US City 2 for zip: ")
 
-    # Get the json response from the request.
-    jsonResponse = get_city_json() if zipOrCityPrompt == "1" else get_zipCode_json()
+        while zipOrCityPrompt != "1" and zipOrCityPrompt != "2":
+            print("Error: Invalid selection")
+            zipOrCityPrompt = input("Would you like to lookup weather data by US City or zip code?\nEnter 1 for US City 2 for zip: ")
 
-    # Get the weather data from the json into an array.
-    weatherDataArray = get_weather_data(jsonResponse)
+        # Get the json response from the request.
+        if(zipOrCityPrompt == "1"):
 
-    print_weather_data(weatherDataArray)
+            # Use Try/except block to catch potential errors.
+            try:
+                jsonResponse = get_city_json()
+            except:
+                print("ERROR: Client-provided city/state combination returned a 404.\nPlease try again.")
+                jsonResponse = get_city_json()
+        else:
+            try:
+                jsonResponse = get_zipCode_json()
+            except:
+                print("ERROR: Client-provided zip code returned a 404.\nPlease try again.")
+                jsonResponse = get_zipCode_json()
+
+        # Get the weather data from the json into an array.
+        weatherDataArray = get_weather_data(jsonResponse)
+
+        print_weather_data(weatherDataArray)
+
+        # Ask the user if they would like to use the application.
+        userInput = input("Would you like to look up weather information today?\nPress 'Y' or 'y' for yes or press any other key to quit. ")
+
+    # Thank the user and exit the application.
+    print("Thank you for using the Weather Application!")
 
 
-
+# Call the main method.
 if __name__ == "__main__":
     main()
 
