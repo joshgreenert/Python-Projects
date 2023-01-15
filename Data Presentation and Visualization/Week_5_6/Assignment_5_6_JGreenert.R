36aa6bc3c1b53a974a0aed501ce0c6642b5d6994
Joshua Greenert <42500582+joshgreenert@users.noreply.github.com>
2023-01-15 18:36:09 -0500

    update

--- Data Presentation and Visualization/Week_5_6/Assignment_5_6_JGreenert.R
+++ Data Presentation and Visualization/Week_5_6/Assignment_5_6_JGreenert.R
@@ -12,12 +12,52 @@ df_unemployment <- read.csv("unemployement-rate-1948-2010.csv")
 # Set up a Tree map chart.
 treemap(df_expenditures, index="category", vSize="expenditure", type="index", title="R Tree Map")
 
+# Set up the variables for the next chart.
+df_unemployment[['Year']] = as.Date(ISOdate(df_unemployment[['Year']], 1, 1), format = "%Y")
+
+# Remove the two bottom rows since they are just the first and second month only.
+df_unemployment <- head(df_unemployment, -2)
+
+# Loop over the data.frame to get the unique years and median of each year
+yearly_averages <- c()
+vector_index <- 0
+
+for(index in 1:nrow(df_unemployment)){
+
+  if(df_unemployment[["Period"]][index] == "M01"){
+    if(index <= 0){
+      total <- 0
+      total <- total + df_unemployment[["Value"]][index]
+    }
+    else{
+      # Get the average for every year.
+      total <- total / 12
+      yearly_averages[vector_index] <-  total
+      
+      # Increment the vector index for added value
+      vector_index <- vector_index + 1
+      
+      total <- 0
+      total <- total + df_unemployment[["Value"]][index]
+    }
+  }
+  else if(df_unemployment[["Period"]][index] != "M12"){
+    total <- total + df_unemployment[["Value"]][index]
+  }
+}
+
+# Add the new values to the new dataframe along with the years.
+new_years <- unique(df_unemployment["Year"])
+new_years <- head(new_years, -1)
+new_df_unemployment = data.frame(new_years ,yearly_averages)
+
 # Set up an area chart
-ggplot(df_unemployment, aes(x=Year, y=Value)) +
-  geom_area( fill="#69b3a2", alpha=2) +
-  geom_line(color="#69b3a2", size=2) +
-  geom_point(size=2, color="#69b3a2") +
-  theme_ipsum() +
+ggplot(new_df_unemployment, aes(x=yearly_averages, y=Year)) +
+  geom_area( fill="#69b3a2") +
   ggtitle("R Area Chart")
 
 # Set up a Stacked area chart
+
+
+
+
